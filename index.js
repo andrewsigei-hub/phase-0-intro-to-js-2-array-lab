473a4dbed62157388e0a91466cd26dfae6d16243
Andrew Sigei <andrew.sigei@student.moringaschool.com>
2025-09-20 00:09:39 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)  {
+  cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name){
+  cats.shift()
+}
+
+function appendCat(name) {
+  return [...cats, name]; // spread operator creates a new array
+}
+
+function prependCat(name) {
+  return [name, ...cats]; // new array with name at the front
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1); // copy without last
+}
+
+function removeFirstCat() {
+  return cats.slice(1); // copy without first
+}
